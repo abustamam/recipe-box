c1262604b87f06144de30be927aea2c554b6d283
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2016-07-13 15:20:55 -0700

    fix undefined bugs

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import AddTodo from '../containers/AddTodo'
 import RecipeListContainer from '../containers/RecipeListContainer'
-import FilterLinks from './FilterLinks'
 
 const Main = () => (
   <div className="main">
-  {/*
-    <AddTodo />
-    <VisibleTodoList />
-    <FilterLinks />
-  */}
     <RecipeListContainer />
   </div>
 )
